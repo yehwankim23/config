601ce3b79753171d0ccf97d619663b2dfcbb1869
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2024-12-23 06:54:42 +0900

    Edit Visual Studio Code Cleanup

--- Windows.bat
+++ Windows.bat
@@ -1,18 +1,18 @@
-@echo off
+@ECHO OFF
 
 @REM Git
-del "%UserProfile%\.gitconfig"
-mklink /h "%UserProfile%\.gitconfig" "Windows\.gitconfig"
-del "%UserProfile%\.gitignore"
-mklink /h "%UserProfile%\.gitignore" "Windows\.gitignore"
+DEL "%UserProfile%\.gitconfig"
+MKLINK /H "%UserProfile%\.gitconfig" "Windows\.gitconfig"
+DEL "%UserProfile%\.gitignore"
+MKLINK /H "%UserProfile%\.gitignore" "Windows\.gitignore"
 
 @REM Git Bash
-del "%UserProfile%\.minttyrc"
-mklink /h "%UserProfile%\.minttyrc" "Windows\.minttyrc"
+DEL "%UserProfile%\.minttyrc"
+MKLINK /H "%UserProfile%\.minttyrc" "Windows\.minttyrc"
 
 @REM Sublime Text
-mkdir "%AppData%\Sublime Text\Packages\User"
-del "%AppData%\Sublime Text\Packages\User\Preferences.sublime-settings"
-mklink /h "%AppData%\Sublime Text\Packages\User\Preferences.sublime-settings" "Windows\Preferences.sublime-settings"
+MKDIR "%AppData%\Sublime Text\Packages\User"
+DEL "%AppData%\Sublime Text\Packages\User\Preferences.sublime-settings"
+MKLINK /H "%AppData%\Sublime Text\Packages\User\Preferences.sublime-settings" "Windows\Preferences.sublime-settings"
 
-pause
+PAUSE
